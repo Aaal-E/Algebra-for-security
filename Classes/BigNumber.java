5b763724f64d7225df8fdd91d11f3fa868f59d32
Alex Thieme <38857430+Aaal-E@users.noreply.github.com>
2018-09-06 12:05:55 +0200

    preliminary structure

---
+++ Classes/BigNumber.java
@@ -0,0 +1,16 @@
+/*
+	stores big numbers as a boolean arraylist
+	maybe add a method to convert to string (in base b)
+*/
+
+class BigNumber() {
+	
+	
+	
+	
+	
+	string toString(int b) {
+		
+	}
+	
+}
