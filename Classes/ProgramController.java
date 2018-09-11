7911afdf720c6995d1f286b3d62ce5ced79d0806
Alex Thieme <38857430+Aaal-E@users.noreply.github.com>
2018-09-11 15:49:31 +0200

    started programcontroller

--- Classes/ProgramController.java
+++ Classes/ProgramController.java
@@ -2,8 +2,17 @@
 
 class ProgramController {
 
-	void run()
-
+	String command;
+	
+	void run() {
+		new Formatter formatter;
+		while(command = formatter.nextInput() != null) {
+			parse(command);
+		}
+	}
+	
+	
+	
 	public static void main(String[] args){
 		new ProgramController().run();
 	}
