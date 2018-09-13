239d0b2fb9444af44874319565f49cd76caab35d
AdrianVramulet <vramuletadrian@yahoo.com>
2018-09-13 13:10:42 +0200

    Update on Multiplier.
    Almost finished. Only have to test if it works correctly.

--- Classes/Integer Arithmatic/Muliplier.java
+++ Classes/Integer Arithmatic/Muliplier.java
@@ -2,42 +2,45 @@ import java.util.*;
 
 class Multiplier {
 	
-	ArrayList <Boolean> multiply (ArrayList <Boolean> number1, ArrayList <Boolean> number2 ){
-	
-		if ( number1.get(number1.size() ) == true) && number2.get(number2.size() ) == true )
+	ArrayList <Integer> multiply (ArrayList <Integer> x, ArrayList <Integer> y, int b ){
 		
-	    //if both numbers are negative, the result is positive
+		int m = x.size();
+		int n = y.size();
+		int k; //size of the result
+		int c, t; //carry and dummy variable t
 		
-		number1.remove (number1.size() - 1); //removing the sign bit for both numbers
-	    number2.remove ( number2.size() -1);
+		ArrayList <Integer> z = new ArrayList <Integer>();
 		
-		ArrayList <Boolean> result = new ArrayList<Boolean>(); //declaring the result
+		for (int i = n; i < m + n -1; i++)
+			z.set(i,0); //reset the resulting array z = xy.
 		
-		for (int i = 0; i < number1.size(); i++)
-			for ( int j = 0; j < number2.size(); j++) {
-			   if(number2.get(j) == false )
-				   
-				   //if I multiply only by 0, then skip, nothing changes
-				   continue;
-			   else
-				if ( number2.get(j) == true && number1.get(i) == true)
-					// 1 . 1 = 1. Corresponds to the index of 
-					//result.set(i+j, (int)(result.get(i+j)) + 1);
-					
-			     		
-			   	   
-			}  
-		        
+		for (int i = 0; i < m; i++) {
+			c = 0; //reset the carry
+			for ( int j = 0; j < n; j++) {
+				
+			   t = z.get(i+j) + x.get(i)*y.get(j) + c; //do the multiplication with carry
+			   
+			   c = Math.floorDiv(t, b);
+			   
+			   z.set(i+j, t - c*b);	
+			}
+			z.set(i+n, c); //store the new carry
+		}
+		
+		if(z.get(m + n - 1) == 0)
+			k = m + n - 2;
+		else
+			k = m + n - 1;
+		
+		if (x.get(m) == 1 || y.get(n) == 1)
+			z.set(k, 1);   //if one of the nrs is negative, the result is negative.
+		else
+			z.set(k, 0);    //if both pos or negative, the result is positive.
+		          
+		return z; // [z]_b in the usual form sum_{0}^{m+n-1} ( z_i* b^{i)
+
 	}
-	
-	
-	
-	
-	
 	public static void main (String [] args ) {
-		
-		
-		
 		
 	}
 }
