994b0bd70c455b24c10ecde82bf4907f153f9cb2
thjhoekstra <thjhoekstra@gmail.com>
2018-09-20 12:10:00 +0200

    added a bit to karatsuba

--- Classes/Integer Arithmatic/Karatsuba.java
+++ Classes/Integer Arithmatic/Karatsuba.java
@@ -2,7 +2,12 @@ import java.util.*;
 
 class Karatsuba {
 	
-	ArrayList<Integer> karatsuba (ArrayList<Integer> x, ArrayList<Integer> y, int b) {
+	ArrayList<Integer> karatsuba (ArrayList<Integer> x, ArrayList<Integer> y, int base) {
+		
+		
+
+		ArrayList<Integer> result  = (a * (B^2m)) + (b * (B^m)) + c
+		
 		return null;
 	}
 	
